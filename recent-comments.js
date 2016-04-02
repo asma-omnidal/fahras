8185f5ce19552025208ffd5f436e75e9b6bcfb6a
omnidal <shahada.alg@hotmail.com>
2016-04-02 16:47:57 +0200

    Added files via upload

---
+++ recent-comments.js
@@ -0,0 +1,19 @@
+/**
+ * Recent Comments Widget with Avatar for Blogger
+ *
+ * @author  th3maestro * @link    http://www.th3maestro.com/
+ * @version 1.3.1
+ *
+ * @example
+ *    var
+ *        numComments    = 5,
+ *        showAvatar     = true,
+ *        avatarSize     = 60,
+ *        roundAvatar    = true,
+ *        showMorelink   = false,
+ *        moreLinktext   = "More &raquo;"
+ *        characters     = 40,
+ *        defaultAvatar  = "http://www.gravatar.com/avatar/?d=mm",
+ *        hideCredits    = false;
+ *
+ */eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('9 D=D||5,6=6||16,o=o||17,k=k||"h://L.M.f/18/?d=19",E=E||" 1a &1b;",p=(q p===\'r\')?j:p,u=(q u===\'r\')?N:u,v=(q v===\'r\')?j:v,w=(q w===\'r\')?N:w;1c O(m){9 7;7=\'<P F="O">\';Q(9 i=0;i<D;i++){9 x,y,2,n;4(i==m.R.b.G)S;7+="<T>";9 b=m.R.b[i];Q(9 l=0;l<b.H.G;l++){4(b.H[l].1d==\'1e\'){x=b.H[l].z;S}}y=b.U[0].1f.$t;2=b.U[0].1g$1h.V;4(2.g("/W/")!=-1){2=2.A("/W/","/s"+6+"-c/")}e 4(2.g("/X/")!=-1){2=2.A("/X/","/s"+6+"-c/")}e 4(2.g("/Y-c/")!=-1&&2.g("h:")!==0){2="h:"+2.A("/Y-c/","/s"+6+"-c/")}e 4(2.g("I.f/B/1i-Z.J")!=-1){2="h://3.10.11.f/-1j-1k/1l/1m/1n/s"+6+"/1o.12"}e 4(2.g("I.f/B/1p-Z.J")!=-1){2="h://3.10.11.f/-1q/1r/1s/1t/s"+6+"/1u.12"}e 4(2.g("I.f/B/1v.J")!=-1){4(k.g("M.f")!=-1){2=k+"&s="+6}e{2=k}}e{2=2}4(p===j){4(v===j){n="1w"}e{n=""}7+="<13 F=\\"1x "+n+"\\"><B F=\\""+n+"\\" V=\\""+2+"\\" 1y=\\""+y+"\\" 1z=\\""+6+"\\" 1A=\\""+6+"\\"/></13>"}7+="<a z=\\""+x+"\\">"+y+"</a>";9 14=b.1B.$t;9 8=14.A(/(<([^>]+)>)/1C,"");4(8!==""&&8.G>o){8=8.1D(0,o);8+="&1E;";4(u===j){8+="<a z=\\""+x+"\\">"+E+"</a>"}}e{8=8}7+="<C>"+8+"</C>";7+="</T>"}7+=\'</P>\';9 K="";4(w===j){K="15:1F;"}7+="<C 1G=\\"1H-1I:1J;15:1K;1L-1M:1N;"+K+"\\">1O 1P <a z=\\"h://L.m.f/\\">m</a></C>";1Q.1R(7)}',62,116,'||authorAvatar||if||avatarSize|commentsHtml|commBody|var||entry|||else|com|indexOf|http||true|defaultAvatar||th3maestro|avatarClass|characters|showAvatar|typeof|undefined|||showMorelink|roundAvatar|hideCredits|commentlink|authorName|href|replace|img|span|numComments|moreLinktext|class|length|link|blogblog|gif|hideCSS|www|gravatar|false|th3maestro_recent_comments|ul|for|feed|break|li|author|src|s1600|s220|s512|rounded|bp|blogspot|png|div|commHTML|display|60|40|avatar|mm|More|raquo|function|rel|alternate|name|gd|image|b16|AaI8|1X32ZM|TxMKLVzQ5BI|AAAAAAAABYY|QYau8ov2blE|th3maestro_blogger_logo|openid16|9lSeVyNRKx0|TxMKMIqMNuI|AAAAAAAABYc|8iasY0xpLzc|th3maestro_openid_logo|blank|avatarRound|avatarImage|alt|width|height|content|ig|substring|hellip|none|style|font|size|10px|block|text|align|right|Widget|by|document|write'.split('|'),0,{}))
